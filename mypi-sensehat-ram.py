d66faecd5470e2f53621f117bffdf801aad092ba
framboise-pi <55726262+framboise-pi@users.noreply.github.com>
2025-07-04 01:54:03 +0300

    Update mypi-sensehat-ram.py
    2025 version

--- mypi-sensehat-ram.py
+++ mypi-sensehat-ram.py
@@ -1,65 +1,62 @@
-#!/usr/bin/env python
-#coding=utf-8
+#!/usr/bin/env python3
+############################################ mpsram : MyPiSensehatRAM
 #
-#*	MyPi-SenseHat-ram
-#*	https://github.com/framboise-pi/MyPi-SenseHat-ram
-#*	Copyright(C) 2020 Cedric Camille Lafontaine http://www.framboise-pi.fr,
-#*	version 0.0.1
+#	Copyright(C) LAFONTAINE CÃ©dric Camille 2020
+#	contact@codelibre.fr
+#	https://github.com/framboise-pi/MyPi-SenseHat-cpu/blob/master/mypi-sensehat-ram.py
 #
-
+#           _        _ _ _             ___
+#  ___ ___ _| |___   | |_| |_ ___ ___  |  _|___
+# |  _| . | . | -_|  | | | . |  _| -_|_|  _|  _|
+# |___|___|___|___|  |_|_|___|_| |___|_|_| |_|
+#
+# ASCII art generator: http://patorjk.com/software/taag/
+#
+########################################################################
+# USAGE EXAMPLE:
+# python3 mypi-sensehat-ram.py
+########################################################################
 from sense_hat import SenseHat
 import random
 from random import randint
 import psutil
 import time
 
-global tour
-
-tour = 0
 sense = SenseHat()
-sense.rotation = 270
+sense.rotation = 0
 sense.low_light = True
 sense.clear()
-
+tour = 0
+DELAY_SEC = 2
 print("...starting SenseHat ram script...")
 
-def PixelRam(tour,ram):
-	green = [0,255,0]
-	y = 0
+def PixelRam(ram):
+	global tour
+	if tour > 7:
+		tour = 0
+		sense.clear()
 	percent = int(ram)
-	if (percent <= 12):
-		y = 7
-		green[0] = 100
-	if (percent > 12 and percent <= 25):
-		y = 6
-		green[0] = 120
-	if (percent > 25 and percent <= 37):
-		y = 5
-		green[0] = 140
-	if (percent > 37 and percent <= 40):
-		y = 4
-		green[0] = 160
-	if (percent > 40 and percent <= 52):
-		y = 3
-		green[0] = 180
-	if (percent > 52 and percent <= 65):
-		y = 2
-		green[0] = 200
-	if (percent > 65 and percent <= 77):
-		y = 1
-		green[0] = 220
-	if (percent > 77 and percent <= 100):
-		y = 0
-	#debug
-	#print ("ram:",percent,"tour:",tour,"pixel h:",y,"bright:",green[0])
-	sense.set_pixel(tour,y,green)
-
+	thresholds = [
+		(0, 12, 7, 100),
+		(12, 25, 6, 120),
+		(25, 37, 5, 140),
+		(37, 40, 4, 160),
+		(40, 52, 3, 180),
+		(52, 65, 2, 200),
+		(65, 77, 1, 220),
+		(77, 100, 0, 255)
+	]
+	y = None
+	for lower, upper, y_value, green_value in thresholds:
+		if lower < percent <= upper:
+			if green_value is not None and int(y_value):
+				green = (0,green_value,0)
+				sense.set_pixel(tour,y_value,green)
+				tour += 1
+				# DEBUG print ("ram_load:",percent,"tour:",tour,"pixel y:",y_value)
+	
 while True:
 	ram = psutil.virtual_memory().percent
-	if (ram is not None):
-		if (tour >= 7):
-			sense.clear()
-			tour = 0
-		else: tour = tour + 1
-		PixelRam(tour,ram)
-		time.sleep(1)
+	if float(ram):
+		PixelRam(ram)
+		time.sleep(DELAY_SEC)
